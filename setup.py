7cc8c05762d8a7d0cb86950fd45df8684d4afac7
eiki <eiki@pop-os.localdomain>
2022-09-08 10:23:36 -0300

    TestPypi

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='dev_aberto_eikily',
+      version='0.1',
+      packages=['dev_aberto'],
+      install_requires = ['requests'],
+      scripts=["scripts/hello.py"]
+      )
