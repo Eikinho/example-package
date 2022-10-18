cacc47bc868831e020bb97a861be57618b049f83
eiki <eiki@pop-os.localdomain>
2022-10-18 11:29:30 -0300

    locale

---
+++ Makefile
@@ -0,0 +1,5 @@
+pt-br: 
+	LANGUAGE=pt_BR.utf8 python3 hello.py
+
+en-us:
+	LANGUAGE=en_US.utf8	python3 hello.py 
--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
-from dev_aberto import hello
+from build.lib import dev_aberto
+from dev_aberto.dev_aberto import hello
+from babel import dates
+from datetime import datetime
+import gettext
+gettext.install('hello', localedir='locale') 
 
 if __name__ == '__main__':
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    format_data = "%Y-%m-%dT%H:%M:%SZ"
+    date = datetime.strptime(date, format_data)
+    
+    last_commit_ = _('Last commit in')
+    by_ = _('by')
+    print(last_commit_, dates.format_date(date, 'full'), by_, name)
